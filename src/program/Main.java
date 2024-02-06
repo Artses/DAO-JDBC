b67c8a023eb3a1500a0eb65ff41df6e30a3e0f4a
Arthur Eustaquio <arthurreustaquio@gmail.com>
2024-02-05 22:54:17 -0300

    Project and git repository

---
+++ src/program/Main.java
@@ -0,0 +1,13 @@
+package program;
+
+import db.DB;
+import db.DbException;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
